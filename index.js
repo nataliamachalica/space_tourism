d2595cb50d03a01fd81cd27f64f317ea60535d9a
Natalia Szlapka <n.machalica@gmail.com>
2022-01-18 13:03:49 +0100

    Add hamburger menu to mobile navigation & functionality to it

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// javascript
+// when someone clicks the hamburger button
+// if the menu is closed, open it
+// if the menu is open, close it
+
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+// when someone clicks the hamburger button
+navToggle.addEventListener('click', () => {
+	// if nav is closed, open it
+	const visibility = nav.getAttribute('data-visible');
+	//if nav is open, close it
+	if (visibility === 'false') {
+		nav.setAttribute('data-visible', true)
+	} else {
+        nav.setAttribute('data-visible', false);
+	}
+});
