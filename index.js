c6e8d242c429bd2f38272d25ca8ff179cd686894
Natalia Szlapka <n.machalica@gmail.com>
2022-01-18 13:46:53 +0100

    Add animation and change menu icon

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// when someone clicks the hamburger button
-// if the menu is closed, open it
-// if the menu is open, close it
-
 const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
 // when someone clicks the hamburger button
 navToggle.addEventListener('click', () => {
+
 	// if nav is closed, open it
 	const visibility = nav.getAttribute('data-visible');
 	//if nav is open, close it
 	if (visibility === 'false') {
-		nav.setAttribute('data-visible', true)
+		nav.setAttribute('data-visible', true);
+		navToggle.setAttribute('aria-expanded', true);
 	} else {
         nav.setAttribute('data-visible', false);
+		navToggle.setAttribute('aria-expanded', false);
 	}
 });
+
