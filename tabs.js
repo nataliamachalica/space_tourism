56aaeb3bf7fc039ec77e06a277619fdd8a9e3c43
Natalia Szlapka <n.machalica@gmail.com>
2022-01-21 14:11:26 +0100

    Make navigating functions clearer

--- tabs.js
+++ tabs.js
@@ -13,27 +13,27 @@ tabs.forEach((tab) => {
     const keydownLeft = 37;
     const keydownRight = 39;
 
-    if(e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-      tabs[tabFocus].setAttribute('tabindex', -1);
-    }
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+      tabs[tabFocus].setAttribute("tabindex", -1);
 
-    if (e.keyCode === keydownRight) {
-      tabFocus++;
-      if (tabFocus >= tabs.length) {
-        tabFocus = 0;
+      if (e.keyCode === keydownRight) {
+        tabFocus++;
+        if (tabFocus >= tabs.length) {
+          tabFocus = 0;
+        }
+      } else if (e.keyCode === keydownLeft) {
+        tabFocus--;
+        if (tabFocus < 0) {
+          tabFocus = tabs.length - 1;
+        }
       }
-    }
 
-    if (e.keyCode === keydownLeft) {
-      tabFocus--;
-      if (tabFocus < 0) {
-        tabFocus = tabs.length -1;
-      }
+      tabs[tabFocus].setAttribute("tabindex", 0);
+      tabs[tabFocus].focus();
     }
+  }
 
-    tabs[tabFocus].setAttribute('tabindex', 0);
-    tabs[tabFocus].focus();
-}
+
 
 function changeTabPanel(e) {
   const targetTab = e.target;
@@ -49,22 +49,19 @@ function changeTabPanel(e) {
 
   targetTab.setAttribute('aria-selected', true);
 
-  mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute("hidden", true));
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
 
-  mainContainer
-    .querySelector([`#${targetPanel}`])
-    .removeAttribute('hidden');
+  hideContent(mainContainer, 'picture');
+  showContent(mainContainer, [`#${targetImage}`]);
+}
 
-  mainContainer
-    .querySelectorAll('picture')
-    .forEach((picture) => picture.setAttribute("hidden", true));
+function hideContent(parent, content){
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute('hidden', true));
+}
 
-  mainContainer
-    .querySelector([`#${targetImage}`])
-    .removeAttribute("hidden");
-
-
-  //console.log(mainContainer);
+function showContent(parent, content){
+  parent.querySelector(content).removeAttribute('hidden');
 }
